bc55c16a1fbd89356f62c3125ea639a9c0034750
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2024-03-14 00:11:51 +0530

    Update db.js

--- back/db.js
+++ back/db.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: '127.0.0.1',
-    database: 'temp',
+    database: 'cms2',
     user: 'root',
     password: ''
 });
